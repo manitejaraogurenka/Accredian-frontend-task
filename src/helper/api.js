f8ed92bd1b096d839635034746881afe2161819a
Maniteja Gurenka <maniteja360@gmail.com>
2024-07-21 14:07:53 +0530

    removed localhost proxy on frontend

--- src/helper/api.js
+++ src/helper/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 export const submitReferral = async (data) => {
   try {
-    const response = await axios.post("/api/referral", data);
+    const response = await axios.post(
+      "https://accredian-backend-api-p2uk.onrender.com/api/referral",
+      data
+    );
     return response;
   } catch (error) {
     throw new Error("Error submitting referral: " + error.message);
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://accredian-backend-api-p2uk.onrender.com/",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  server: {},
 });
