eb08d297d1ba89377854a67a41a510a55ef4492f
Maniteja Gurenka <maniteja360@gmail.com>
2024-07-21 13:30:32 +0530

    updated connection to api

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://accredian-backend-api-p2uk.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
